c32e53f713e21d210903b20fc044478d93d313a3
dispector <dmitry_proekt@mail.ru>
2020-03-10 02:46:47 +0300

    add: done task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = 'undefined') {
+  if (array == 'undefined' || array.length == 0){
+    return 0;
+  }
+  return Math.min(...array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = 'undefined') {
+  if (array == 'undefined' || array.length == 0){
+    return 0;
+  }
+  return Math.max(...array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = 'undefined') {
+  if (array == 'undefined' || array.length == 0){
+    return 0;
+  }
+  let sum = array.reduce((a, b) => a + b, 0);
+  let avg = (sum / array.length);
+  return avg;
 }
